fffb47e50a27ea9bb5f53124e0b7fed257824623
KokiWa <125528969+KokiWa@users.noreply.github.com>
2023-02-17 13:19:31 +0900

    Add files via upload

--- omikuji.js
+++ omikuji.js
@@ -2,6 +2,10 @@ let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
 document.getElementById("name").innerHTML = username;
+if (username == "");
+{
+    name = "";
+}
 
 let rand = Math.floor( Math.random() * 5) ;
 if (rand == 0) {
